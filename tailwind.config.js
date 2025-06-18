9e0abd3ad1e94a474d3aab26b4c3e3517b1f408f
Nagbhushan_Pai <nagbhushan.mitmpl2022@learner.manipal.edu>
2025-06-18 13:51:17 +0530

    setup styling complete

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,22 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#1DA1F2',
+        secondary: '#14171A',
+        light:{
+          100: '#F5F8FA',
+          200: '#E1E8ED',
+          300: '#AAB8C2',
+        },
+        dark: {
+          100: '#657786',
+          200: '#AAB8C2',
+          300: '#E1E8ED',
+        },
+      },
+    },
   },
   plugins: [],
 }
