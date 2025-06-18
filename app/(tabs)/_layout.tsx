b1ba1e5536c1b85fb8548ca509e617aebf8bcfc1
Nagbhushan_Pai <nagbhushan.mitmpl2022@learner.manipal.edu>
2025-06-18 14:20:52 +0530

    hide all tabs

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,19 @@
+import {View,Text,StyleSheet} from "react-native";
+import React from "react";
+import {Tabs} from "expo-router";
+
+const _Layout = () => {
+  return (
+    <Tabs>
+        <Tabs.Screen
+          name="index"
+          options={{
+            title: "Home",
+            headerShown: false,
+          }}
+        />
+    </Tabs>
+      
+  );
+};
+export default _Layout;
